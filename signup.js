73ce256f3f4f1994dbba6ad6e570f01cb7440c72
ARUNAW RISHE <arunawrishe@gmail.com>
2025-02-19 16:01:52 +0530

    dai summa

---
+++ signup.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded", function () {
+    document.getElementById("signupButton").addEventListener("click", function () {
+        window.location.href = "home.html";  // Redirects to home.html
+    });
+});
+
